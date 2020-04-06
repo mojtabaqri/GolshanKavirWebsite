08a6211b0028874fd8da1401bb194845dcab8f4e
mojtabaqri <mojtabaqri@gmail.com>
2020-04-06 18:59:53 +0430

    ناور بار چسبنده اضافه شد
    18 فروردین 99

--- js/controller.js
+++ js/controller.js
@@ -15,5 +15,16 @@ $(".item-holder").owlCarousel({
         owl.trigger('prev.owl.carousel', [300]);
     })
     //-----------------------------------------------Slider--------------------------------------------
-
+//-------------------- Sticky Nav
+    $(document).scroll(function () {
+     let percent=12;
+     let scrollFromTop=$(this).scrollTop();
+     let middle=$(document).height()*percent/100;
+     console.log(middle);
+     if(scrollFromTop > middle)
+         $(".sticky-nav").show("2000");
+     else
+         $(".sticky-nav").hide("2000");
+    })
+//-------------------- Sticky Nav
 });
